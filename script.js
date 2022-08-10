435072337f7fb3454deae9a25e6626605d209fb6
Federico Valdiglesias <federicomrtl@gmail.com>
2022-08-10 18:36:46 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,44 @@
+/* 
+calculadora de IMC
+    Establezca valores para la altura en cm y el peso en gramos, luego convierta los valores a 
+    metros y kilos, enviando los resultados a la consola:
+        1 cm es igual a 0.01 m
+        1 kilo es igual a 1000 gramos
+
+    Salida de los resultados. Luego, calcula y registra el IMC: esto es igual al peso (en kilos) 
+    dividido por la altura al cuadrado (en metros). Envíe los resultados a la consola.
+
+ */
+
+// let pesoUsuario = prompt("Ingrese su peso en gr." );
+
+// let alturaUsuario = prompt("Ingrese su altura en cm.");
+
+// let conversionPeso = (pesoUsuario/1000);
+// let conversionAltura = (alturaUsuario * 0.01);
+
+// let imc = (conversionPeso/(conversionAltura)**2);
+
+// alert('Su indice de masa corporal es: ' + imc );
+
+/* ---------- Condicionales: TAREA ----------------------
+
+Evaluación de un juego de números
+
+Pida al usuario que ingrese un número y verifique si es mayor, igual 
+o menor que un valor de número dinámico en su código. Envía el resultado al usuario.
+
+
+ */
+
+// let numeroCorrecto = 12;
+
+// let numeroUsuario = prompt("Ingrese un numero por favor");
+
+// if (numeroUsuario > numeroCorrecto) {
+//   alert("El numero ingresado es mayor al numero correcto");
+// } else if (numeroUsuario < numeroCorrecto) {
+//   alert("El numero ingresado es menor al numero correcto");
+// } else {
+//   alert("Numero acertado!!!");
+// }
